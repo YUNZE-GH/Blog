85615a1b568db2fbfa4d8dcc01e6583fe19f31bf
gaoh <834363368@qq.com>
2020-08-18 18:14:51 +0800

    实现swagger接口API文档可视化

---
+++ src/main/java/com/gh/blog/config/ServletContextConfig.java
@@ -0,0 +1,30 @@
+package com.gh.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * WebMvc配置
+ * @author gaohan
+ * @version 1.0
+ * @date 2020/7/30 17:54
+ */
+@Configuration
+public class ServletContextConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
