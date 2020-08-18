2723d8584746c105cb2637c06c345af5662e5d9c
gaoh <834363368@qq.com>
2020-08-18 22:28:03 +0800

    添加扫描forest框架包注解

--- src/main/java/com/gh/blog/BlogApplication.java
+++ src/main/java/com/gh/blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package com.gh.blog;
 
+import com.thebeastshop.forest.springboot.annotation.ForestScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +13,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @date 2020/8/11 21:11
  */
 @SpringBootApplication
+// 表示扫描dao包下的所有类，将其加入到IOC容器，也就是说dao包下的类可以不加@Repository注解
 @MapperScan("com.gh.blog.dao")
+@ForestScan(basePackages = "com.gh.blog.client")
 public class BlogApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
