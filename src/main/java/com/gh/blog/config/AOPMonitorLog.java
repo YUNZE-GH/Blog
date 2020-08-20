ccb00180b0666201c57b022d64feedfe4580c56b
gaoh <834363368@qq.com>
2020-08-20 20:52:43 +0800

    使用AOP实现日志监控

---
+++ src/main/java/com/gh/blog/config/AOPMonitorLog.java
@@ -0,0 +1,54 @@
+package com.gh.blog.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * @author gaohan
+ * @version 1.0
+ * @date 2020/8/20 20:21
+ */
+@Aspect
+@Component
+public class AOPMonitorLog {
+    private Log log = LogFactory.getLog(this.getClass().getName());
+    /**
+     * 切入点
+     * 匹配com.example.controller1包及其子包下的所有类的所有方法
+     */
+    @Pointcut("execution(public * com.gh.blog.controller..*.*(..))")
+    public void executePackage() {
+
+    }
+        // 在切入点的方法run之前要干的
+    @Before("executePackage()")
+    public void before(JoinPoint joinPoint){
+        Signature signature = joinPoint.getSignature();
+        log.info("------------------" + signature.getName() + "方法执行前-------------------");
+        log.info(signature);
+        Object[] obj = joinPoint.getArgs();
+        log.info(Arrays.asList(obj));
+    }
+
+    @After("executePackage()")
+    public void after(JoinPoint joinPoint){
+        Signature signature = joinPoint.getSignature();
+        log.info("------------------" + signature.getName() + "方法执行后-------------------");
+    }
+
+
+}
